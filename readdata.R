ad36b80ff7562f5b36ba6ab792bb65c064682e1a
Jingmin24 <janus24zhang@hotmail.com>
2017-06-22 14:55:04 +0200

    Update and rename readdata to readdata.R

--- readdata.R
+++ readdata.R
@@ -2,3 +2,7 @@ df<-read.table("default of credit card clients.csv",T,stringsAsFactors = F,sep="
 colnames(df)
 df[,c(3,4,5,7:12,25)] = lapply(df[,c(3,4,5,7:12,25)],as.factor)
 summary(df)
+
+####deal with the uncorrect value in "Education" and "Marriage",use mode to replace###
+falseMR<-which(df$MARRIAGE==0)
+df$MARRIAGE[falsedMR]<-2
