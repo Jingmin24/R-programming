0ce4e8e328131fec2fd5f748c02f75bbd1e97709
Jingmin24 <janus24zhang@hotmail.com>
2017-06-27 13:36:15 +0200

    Update readdata.R

--- readdata.R
+++ readdata.R
@@ -1,8 +1,13 @@
+####read data###
 df<-read.table("default of credit card clients.csv",T,stringsAsFactors = F,sep=",",skip=1)
-colnames(df)
-df[,c(3,4,5,7:12,25)] = lapply(df[,c(3,4,5,7:12,25)],as.factor)
+df[,c(3,5,7:12,25)] = lapply(df[,c(3,5,7:12,25)],as.factor)
 summary(df)
 
-####deal with the uncorrect value in "Education" and "Marriage",use mode to replace###
+####deal with the uncorrect value in "education" and "Marriage" using the most frequent value#### 
+median.edu =median(df$EDUCATION)
+df$EDUCATION = ifelse(df$EDUCATION>4 | df$EDUCATION==0, median.edu,df$EDUCATION)
+###convert into factor####
+df$EDUCATION<-as.factor(df$EDUCATION)
+
 falseMR<-which(df$MARRIAGE==0)
 df$MARRIAGE[falsedMR]<-2
