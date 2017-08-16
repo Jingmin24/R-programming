cdbe0bc1fe991225791cf86479b00f597c61181a
YiyingZhang <zhangyiying321@hotmail.com>
2017-08-16 12:04:50 +0200

    Update ROC and AUC Results.R

--- SPL_Logit Regression and Naive Bayes/ROC and AUC Results.R
+++ SPL_Logit Regression and Naive Bayes/ROC and AUC Results.R
@@ -3,7 +3,7 @@
 if(!require("hmeasure"))install.packages("hmeasure");library("hmeasure")
 
 predictions.roc.all<-data.frame(nb=pred.nb[,2],lr=pred.lr)
-n.test<-as.numeric(test$default.payment.next.month)-1
+n.test<-as.numeric(test$default.payment.next.month)
 h<-HMeasure(n.test,predictions.roc.all)
 plotROC(h,which = 1)
 
@@ -14,5 +14,5 @@ tau<-0.5
 lr.class<-factor(yhat.validation$lr>tau,labels = c("0","1"))
 nb.class<-factor(yhat.validation$nb>tau,labels=c("0","1"))
 
-confusionMatrix(data=lr.class,reference = test$default.payment.next.month,positive = "0")#0.8196
-confusionMatrix(data=nb.class,reference = test$default.payment.next.month,positive = "0")#0.7081
+confusionMatrix(data=lr.class,reference = test$default.payment.next.month,positive = "0")
+confusionMatrix(data=nb.class,reference = test$default.payment.next.month,positive = "0")
